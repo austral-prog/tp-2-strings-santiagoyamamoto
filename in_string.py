8be441420737b80b8ff2e293bdfea661f0b59c52
santiagoyamamoto <santiagoyamamoto08@gmail.com>
2026-03-18 00:25:49 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -6,8 +6,8 @@ def check_vowels():
 
     nombre=input()
     min=nombre.lower()
-    print(f"Contiene a: {"a" in nombre}")
-    print(f"Contiene e: {"e" in nombre}")
-    print(f"Contiene i: {"i" in nombre}")
-    print(f"Contiene o: {"o" in nombre}")
-    print(f"Contiene u: {"u" in nombre}")
+    print(f"Contiene a: {'a' in nombre}")
+    print(f"Contiene e: {'e' in nombre}")
+    print(f"Contiene i: {'i' in nombre}")
+    print(f"Contiene o: {'o' in nombre}")
+    print(f"Contiene u: {'u' in nombre}")
