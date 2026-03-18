225648f483af7358f531b4780618e1bb243cab43
santiagoyamamoto <santiagoyamamoto08@gmail.com>
2026-03-18 00:27:45 -0300

    Update string_methods.py

--- string_methods.py
+++ string_methods.py
@@ -9,8 +9,8 @@ def string_methods():
     nombre = "   Grace Hopper   "
     frase = "Python es un gran lenguaje de programacion"
     multilinea = """Linea 1
-    Linea 2
-    Linea 3"""
+Linea 2
+Linea 3"""
 
     print(f"Strip: {nombre.strip()}")
     print(f"Lstrip: {nombre.lstrip()}")
@@ -18,11 +18,11 @@ def string_methods():
     print(f"Upper: {frase.upper()}")
     print(f"Lower: {frase.lower()}")
     print(f"Title: {frase.title()}")
-    print(f"Find: {frase.find("gran")}")
-    print(f"Replace: {frase .replace("programacion", "desarrollo")}")
-    print(f"Count: {frase.count("a")}")
-    print(f"Contiene Python: {"Python" in frase}")
-    print(f"Contiene Java: {"Java" in frase}")
+    print(f"Find: {frase.find('gran')}")
+    print(f"Replace: {frase .replace('programacion', 'desarrollo')}")
+    print(f"Count: {frase.count('a')}")
+    print(f"Contiene Python: {'Python' in frase}")
+    print(f"Contiene Java: {'Java' in frase}")
     print(f"Slice: {frase[:6]}")
     print(f"Paso: {frase[0:5:2]}")
     print(f"Reverso: {frase[5::-1]}")
