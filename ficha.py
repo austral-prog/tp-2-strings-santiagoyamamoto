f12ae4038bbd93257181e722f3c6286679887cfb
santiagoyamamoto <santiagoyamamoto08@gmail.com>
2026-03-17 23:55:29 -0300

    Update ficha.py

--- ficha.py
+++ ficha.py
@@ -1,3 +1,6 @@
+from gettext import find
+
+
 def ficha():
     """Ejercicio integrador. Lee nombre, email y 3 notas, y genera una ficha
     de alumno aplicando: strip, title, lower, upper, int, len, find, slicing,
@@ -25,3 +28,42 @@ def ficha():
     #   - Las 3 notas, su suma, promedio y promedio entero
     #   - Cierre decorativo usando repetición de string ("=" * 24)
     pass
+
+    nombre=input("Ingrese su nombre completo: ").lower()
+    email=input("Ingrese su email: ")
+    nota1= int(input("Ingrese la primer nota: "))
+    nota2 = int(input("Ingrese la segunda nota: "))
+    nota3 = int(input("Ingrese la tercer nota: "))
+
+    multilinea= """========================
+    FICHA DEL ALUMNO
+========================"""
+
+    print(multilinea)
+    print(f"Nombre: {nombre.strip().title()}")
+    print(f"Email: {email.strip().lower()}")
+    print(f"Caracteres en nombre: {len(nombre)}")
+
+    lugar= int(nombre.find(" "))
+
+    print(f"Iniciales: {nombre[0].upper()+nombre[lugar+1].upper()}")
+    print(f"Usuario: {nombre.lower()[lugar+1:] + "." + nombre.lower()[:lugar] }")
+    print(f"Email valido: {"@" in email}")
+
+    arroba= email.find("@")
+
+    print(f"Dominio: {email[arroba+1:].lower()}")
+    print(f"Nombre para archivo: {nombre.replace(" ", "_").title()}")
+    print(f"Cantidad de a: {nombre.count("a")}")
+    print(f"Codigo secreto: {nombre[::-1].upper()}")
+    print(f"Nota 1: {nota1}")
+    print(f"Nota 2: {nota2}")
+    print(f"Nota 3: {nota3}")
+
+    suma= nota1 + nota2 + nota3
+
+    print(f"Suma: {suma}")
+    print(f"Promedio: {suma/3}")
+    print(f"Promedio entero: {suma//3}")
+    print("="*24)
+
