0e834d9ec1c3a0632849ff07a3cb40697f603361
santiagoyamamoto <santiagoyamamoto08@gmail.com>
2026-03-17 23:57:14 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -2,4 +2,8 @@ def slice_simple():
     """Dado el texto 'Awesome', imprime distintos substrings
     usando slicing y lower().
     """
-    texto = "Awesome"
+    texto= "Awesome"
+
+    print(texto[:3].lower())
+    print(texto[2:5])
+    print(texto.lower())
